2d0d2f051a7f75aaebfb19aded2f68541a27eff9
Justin Stitt <jkstitt@csu.fullerton.edu>
2021-12-10 10:38:11 -0800

    ✏ initial print of csv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('some.csv', newline='') as csvfile:
+with open('./inp/test.csv', newline='') as csvfile:
     rdr = csv.reader(csvfile, delimiter = ',')
     for row in rdr:
         print(', '.join(row)) #  collat rows
