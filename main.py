dd83a9398dac65d2316a920f418ccb4c03f6d959
AaronLieb <aarnliebrmn@gmail.com>
2021-12-10 10:47:10 -0800

    parse files in directory

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import csv
+import os
 
-with open('./inp/test.csv', newline='') as csvfile:
-    rdr = csv.reader(csvfile, delimiter = ',')
-    for row in rdr:
-        print(', '.join(row)) #  collat rows
+files = os.listdir('./reports')
+for file_name in files:
+    print(file_name)
+    if (file_name[-3:] != "csv"): continue
+    with open('./reports/{a}'.format(a=file_name), newline='') as csvfile:
+        rdr = csv.reader(csvfile, delimiter = ',')
+        for row in rdr:
+            print(','.join(row)) #  collat rows
