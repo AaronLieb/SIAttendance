142d1da8cff871df1404d1a1cbbcde486b2e1550
AaronLieb <aarnliebrmn@gmail.com>
2021-12-10 11:46:48 -0800

    track sessions attended instead of minutes

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import csv
 from tkinter import filedialog as fd
 import os
 
-times = {}
+sessions_attended = {}
 
 csv_folder_dir = './reports'
 #csv_folder_dir = fd.askdirectory()
 files = os.listdir(csv_folder_dir)
+
 for file_name in files:
     if (file_name[-3:] != "csv"): continue
     with open(csv_folder_dir + '/' + file_name, newline='') as csvfile:
@@ -14,19 +15,21 @@ for file_name in files:
         next(rdr)
         for row in rdr:
             email = str(row[1])
-            if email in times:
-                times[email][1] += int(row[2])
+            if int(row[2]) < 10:
+                continue
+            if email in sessions_attended:
+                sessions_attended[email][1] += 1
             else:
-                times[email] = (str(row[0]), int(row[2]))
-    for p1 in times:
-        for p2 in times:
+                sessions_attended[email] = (str(row[0]),1 ) 
+    for p1 in sessions_attended:
+        for p2 in sessions_attended:
             if p1 is p2:
                 continue
-            if (times[p1][0] == times[p2][0]):
-                merge = str(input('Duplicate names found, are these the same person? (Y/N): \n {a} \n {b} \n {c}\n'.format(a=times[p1][0],b=p1,c=p2)))
+            if (sessions_attended[p1][0] == sessions_attended[p2][0]):
+                merge = str(input('Duplicate names found, are these the same person? (Y/N): \n {a} \n {b} \n {c}\n'.format(a=sessions_attended[p1][0],b=p1,c=p2)))
                 if (merge.lower() == 'y'):
-                    times[p1][1] += times[p2][1]
-                    times.pop(p2)
+                    sessions_attended[p1][1] += sessions_attended[p2][1]
+                    sessions_attended.pop(p2)
 
 
         
