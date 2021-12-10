0325c1e1e2220a2bdd3a623b054522a3586b0c40
AaronLieb <aarnliebrmn@gmail.com>
2021-12-10 12:29:56 -0800

    finishing touches

--- main.py
+++ main.py
@@ -4,25 +4,29 @@ import os
 
 sessions_attended = {}
 
-default_prof = input()
+default_prof = input('Enter the name of the professor: ')
 
-csv_folder_dir = './reports'
-#csv_folder_dir = fd.askdirectory()
+#csv_folder_dir = './reports'
+csv_folder_dir = fd.askdirectory()
 files = os.listdir(csv_folder_dir)
 
 for file_name in files:
     if (file_name[-3:] != "csv"): continue
     with open(csv_folder_dir + '/' + file_name, newline='') as csvfile:
         rdr = csv.reader(csvfile, delimiter = ',')
-        next(rdr)
+        headers = next(rdr)
+        minute_column = None
+        for header in headers:
+            if 'Duration' in header:
+                minute_column = headers.index(header)
         for row in rdr:
             email = str(row[1])
-            if int(row[2]) < 10:
+            if int(row[minute_column]) < 10:
                 continue
             if email in sessions_attended:
                 sessions_attended[email][1] += 1
             else:
-                sessions_attended[email] = (str(row[0]),1 ) 
+                sessions_attended[email] = [str(row[0]), 1] 
     for p1 in sessions_attended:
         for p2 in sessions_attended:
             if p1 is p2:
@@ -31,20 +35,21 @@ for file_name in files:
                 merge = str(input('Duplicate names found, are these the same person? (Y/N): \n {a} \n {b} \n {c}\n'.format(a=sessions_attended[p1][0],b=p1,c=p2)))
                 if (merge.lower() == 'y'):
                     sessions_attended[p1][1] += sessions_attended[p2][1]
-                    sessions_attended.pop(p2)
+                    sessions_attended[p2][1] = 0
 
 def dict_to_csv(write_file, dict_data):
     with open(write_file, 'w') as towrite:
         writer = csv.writer(towrite)
         for item in dict_data.items():
             email = item[0]
-            name, minutes = item[1] # change min to sesh
+            name, sessions = item[1] 
             name_list = name.split(' ')
             name_list.reverse()
             formatted_name = ', '.join(name_list)
 
-            writer.writerow((formatted_name, default_prof, minutes))
-#        writer.writerows(dict_data.items())
+            if sessions <= 0:
+                continue
+            writer.writerow((formatted_name, default_prof, '', sessions))
 
 if __name__ == '__main__':
-    dict_to_csv('./output.csv', times)
+    dict_to_csv('./output.csv', sessions_attended)
