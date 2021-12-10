01a52cbf51b310065eae099ae1922a645f0e34b8
AaronLieb <aarnliebrmn@gmail.com>
2021-12-10 11:01:54 -0800

    defaul chosen dir to reports

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import csv
 from tkinter import filedialog as fd
-csv_folder_dir = fd.askdirectory()
 import os
 
-files = os.listdir(csv_folder_dir)
+
+times = dict[str, tuple[str,int]]
+
+#csv_folder_dir = fd.askdirectory()
+#files = os.listdir(csv_folder_dir)
+files = os.listdir('./reports')
 for file_name in files:
     print(file_name)
     if (file_name[-3:] != "csv"): continue
     with open(csv_folder_dir + '/' + file_name, newline='') as csvfile:
         rdr = csv.reader(csvfile, delimiter = ',')
+        rdr.next()
         for row in rdr:
             print(','.join(row)) #  collat rows
+            if row[1] in times:
+                times[row[1]][1] += int(row[2])
+            else:
+                times[row[1]][0] = row[0]
+                times[row[1]][1] = row[2]
+
+
+
+                
