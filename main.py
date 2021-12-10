349c65d94f0e2bbd2453ae9f875509f315c42853
Justin Stitt <jkstitt@csu.fullerton.edu>
2021-12-10 10:59:51 -0800

    ✒ added generic file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ files = os.listdir(csv_folder_dir)
 for file_name in files:
     print(file_name)
     if (file_name[-3:] != "csv"): continue
-    with open('./reports/{a}'.format(a=file_name), newline='') as csvfile:
+    with open(csv_folder_dir + '/' + file_name, newline='') as csvfile:
         rdr = csv.reader(csvfile, delimiter = ',')
         for row in rdr:
             print(','.join(row)) #  collat rows
